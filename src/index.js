d59174f010df75c3bfd4e8d2bf9f1678de90b2fa
Alexandr <aleksandr.m@devs.ae>
2024-03-28 14:02:45 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import patientRoutes from './route/patient.route.js';
+import HttpStatus from './controller/patient.controller.js';
+import Response from './domain/response.js';
+import logger from './util/logger.js';
+
+dotenv.config();
+const PORT = process.env.SERVER_PORT || 3000;
+const app = express();
+
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+
+app.use('/patients', patientRoutes);
+
+app.get('/', (req, res) => {
+  res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0 - All Systems Go'));
+});
+
+app.all('*', (req, res) => {
+  res.send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route does not exist on the server'));
+});
+
+app.listen(PORT, () => logger.info(`Server is running on ${PORT} port`));
