e47be65b67488ab1a43d7eda36547ffc449cb786
Alexandr <aleksandr.m@devs.ae>
2024-03-29 17:13:32 +0400

    feat: create/delete elastic index endpoints created feat: find in elastic document endpoint created

--- src/controller/elastic.controller.js
+++ src/controller/elastic.controller.js
@@ -1,6 +1,71 @@
-import { elasticHealth } from '../config/elastic.config.js';
+import util from 'util';
+import { elasticConnector, elasticHealth } from '../config/elastic.config.js';
+import database from '../config/mysql.config.js';
+import QUERY from "../query/patient.query.js";
+
+// node native promisify
+const query = util.promisify(database.query).bind(database);
+
+const ELASTIC_INDEX = 'patients_index';
 
 export const getElasticHealth = async (req, res) => {
   const elasticResponse = await elasticHealth();
   res.send(elasticResponse);
 }
+
+export const setElasticIndex = async (_, res) => {
+  const patientsDataset = await query(QUERY.SELECT_PATIENTS);
+  await elasticConnector.indices.create({ index: ELASTIC_INDEX });
+  const operations = patientsDataset.flatMap((patient) => [{ index: { _index: ELASTIC_INDEX } }, patient]);
+  const bulkResponse = await elasticConnector.bulk({ refresh: true, operations });
+
+  if (bulkResponse.errors) {
+    const erroredDocuments = [];
+    bulkResponse.items.forEach((action, i) => {
+      const operation = Object.keys(action)[0];
+
+      if (action[operation].error) {
+        erroredDocuments.push({
+          status: action[operation].status,
+          error: action[operation].error,
+          operation: operations[i * 2],
+          document: operations[i * 2 + 1],
+        });
+      }
+    });
+
+    console.log('erroredDocuments :: ', erroredDocuments);
+  }
+
+  const count = await elasticConnector.count({ index: ELASTIC_INDEX });
+
+  res.send(count);
+};
+
+export const deleteElasticIndex = async (_, res) => {
+  await elasticConnector.indices.delete({ index: ELASTIC_INDEX });
+  res.send('Removed');
+};
+
+export const findElastic = async (req, res) => {
+  const searchTerm = req.query.search;
+
+  const result = await elasticConnector.search({
+    index: ELASTIC_INDEX,
+    query: {
+      fuzzy: {
+        first_name: {
+          value: searchTerm,
+          fuzziness: 'AUTO',
+        },
+      },
+      /*multi_match: {
+        query: searchTerm,
+        fields: ["first_name", "last_name"],
+        fuzziness: 'AUTO',
+      },*/
+    },
+  });
+
+  res.send(result.hits.hits);
+};
--- src/route/elastic.route.js
+++ src/route/elastic.route.js
@@ -1,9 +1,17 @@
 import express from 'express';
 
-import { getElasticHealth } from '../controller/elastic.controller.js';
+import {
+  deleteElasticIndex,
+  getElasticHealth,
+  findElastic,
+  setElasticIndex,
+} from '../controller/elastic.controller.js';
 
 const elasticRoutes = express.Router();
 
 elasticRoutes.route('/health').get(getElasticHealth);
+elasticRoutes.route('/index').post(setElasticIndex);
+elasticRoutes.route('/index').delete(deleteElasticIndex);
+elasticRoutes.route('/find').get(findElastic);
 
 export default elasticRoutes;
